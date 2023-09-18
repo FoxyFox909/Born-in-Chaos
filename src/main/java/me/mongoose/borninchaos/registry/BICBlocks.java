f8335c0a9187ede99cd181cfd2d9f74dadc73421
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-09-18 18:32:23 -0400

    Initial commit

---
+++ src/main/java/me/mongoose/borninchaos/registry/BICBlocks.java
@@ -0,0 +1,12 @@
+package me.mongoose.borninchaos.registry;
+
+import me.mongoose.borninchaos.BornInChaosMain;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class BICBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, BornInChaosMain.MOD_ID);
+
+}
